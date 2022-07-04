2a146b9bd5ed4b5a9c4b26e078fe65957d555a4f
Kateryna Starynska <katerynastarynska@gmail.com>
2022-07-04 11:07:14 -0500

    new update 03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,37 @@
 import throttle from 'lodash.throttle';
 
-const FEEDBACK_KEY = "feedback-form-state";
+const FEEDBACK_KEY = 'feedback-form-state';
 const formEl = document.querySelector('.feedback-form');
 const textareaInput = document.querySelector('.feedback-form  textarea');
 const emailInput = document.querySelector('.feedback-form  input');
 const formFeedbackData = {};
 
 formEl.addEventListener('input', throttle(onFormInput, 500));
-formEl.addEventListener("submit", handleSubmit);
+formEl.addEventListener('submit', handleSubmit);
 
 function onFormInput(event) {
     formFeedbackData[event.target.name] = event.target.value;
     localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formFeedbackData));
-  }
-  populateFeedbackData();
-  function populateFeedbackData() {
-    const feedbackSavedData = localStorage.getItem(FEEDBACK_KEY);
-    const feedbackParsedData = JSON.parse(feedbackSavedData);
-        if (feedbackParsedData) {
-         emailInput.value = feedbackParsedData.email;
-         textareaInput.value = feedbackParsedData.message;
-    
-         formFeedbackData.email = feedbackParsedData.email;
-         formFeedbackData.message = feedbackParsedData.message;
+}
+populateFeedbackData();
 
-  }}
+function populateFeedbackData() {
+    const feedbackSavedData = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+
+    if (feedbackSavedData && Object.keys(feedbackSavedData).length !== 0) {
+        if (feedbackSavedData.email !== undefined) {
+            emailInput.value = feedbackSavedData.email;
+            formFeedbackData.email = feedbackSavedData.email;
+        }
+        if (feedbackSavedData.message !== undefined) {
+            textareaInput.value = feedbackSavedData.message;
+            formFeedbackData.message = feedbackSavedData.message;
+        }
+    }
+}
 function handleSubmit(event) {
     event.preventDefault();
+    console.log('>>>> data >>>>', formFeedbackData);
     event.currentTarget.reset();
-    console.log(formFeedbackData);
     localStorage.removeItem(FEEDBACK_KEY);
 }
